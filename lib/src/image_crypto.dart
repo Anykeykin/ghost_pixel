361adcb97e2d9132a56c4f5f568dd7ebd530bcdc
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 02:56:56 +0300

    add image encrypt method

---
+++ lib/src/image_crypto.dart
@@ -0,0 +1,35 @@
+import 'dart:typed_data';
+
+import 'package:image/image.dart';
+
+class ImageCrypto{
+  static void imageEncrypt(Image image,Uint8List fileBytes){
+    int byteIndex = 0;
+    int bitIndex = 0;
+
+    for (var y = 0; y < image.height; y++) {
+      for (var x = 0; x < image.width; x++) {
+        final pixel = image.getPixel(x, y);
+
+        final a = pixel.a.toInt();
+        final r = pixel.r.toInt();
+        final g = pixel.g.toInt();
+        final b = pixel.b.toInt();
+
+        if (byteIndex < fileBytes.length) {
+          final bit = (fileBytes[byteIndex] >> (7 - bitIndex)) & 1;
+
+          final newR = (r & 0xFE) | bit;
+          bitIndex++;
+
+          if (bitIndex == 8) {
+            bitIndex = 0;
+            byteIndex++;
+          }
+
+          image.setPixel(x, y, ColorFloat64.rgba(newR, g, b, a));
+        }
+      }
+    }
+  }
+}
