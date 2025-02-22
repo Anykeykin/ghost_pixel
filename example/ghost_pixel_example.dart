f5fbcb20b4e75e8d088634feea57aa77f458d755
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 00:13:54 +0300

    add first example

--- example/ghost_pixel_example.dart
+++ example/ghost_pixel_example.dart
@@ -1,6 +1,22 @@
+import 'dart:io';
+
 import 'package:ghost_pixel/ghost_pixel.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final imagePath = 'yourPath.jpg'; 
+  final filePath = 'yourPath2.txt';
+  final outputImagePath = 'yourPath3.jpg'; 
+  final extractedFilePath = 'yourPath4.txt';
+
+  await GhostPixel.hideFileInImage(
+    imagePath: imagePath,
+    filePath: filePath,
+    outputImagePath: outputImagePath,
+  );
+
+  await GhostPixel.extractFileFromImage(
+    imagePath: outputImagePath,
+    outputFilePath: extractedFilePath,
+    fileSize: File(filePath).lengthSync(),
+  );
 }
