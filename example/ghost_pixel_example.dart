116e5c4da0c8f31d54a47f4024ffd12fa3388073
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 00:35:52 +0300

    ref

--- example/ghost_pixel_example.dart
+++ example/ghost_pixel_example.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 import 'package:ghost_pixel/ghost_pixel.dart';
 
 void main() async {
-  final imagePath = 'yourPath.jpg'; 
+  final imagePath = 'yourPath.jpg';
   final filePath = 'yourPath2.txt';
-  final outputImagePath = 'yourPath3.jpg'; 
+  final outputImagePath = 'yourPath3.jpg';
   final extractedFilePath = 'yourPath4.txt';
 
   await GhostPixel.hideFileInImage(
