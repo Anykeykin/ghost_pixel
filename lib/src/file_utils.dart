0bc47516bf21ccb981df364e63d4903dc3fc6221
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 19:43:43 +0300

    ref

--- lib/src/file_utils.dart
+++ lib/src/file_utils.dart
@@ -4,12 +4,12 @@ import 'dart:typed_data';
 import 'package:image/image.dart';
 
 class FileUtils {
-  static Future<Image?> getImage(Uint8List imageBytes) async{
+  static Future<Image?> getImage(Uint8List imageBytes) async {
     return decodeImage(imageBytes);
   }
 
-  static Future<Uint8List> getFileBytes(String imagePath) async{
+  static Future<Uint8List> getFileBytes(String imagePath) async {
     final imageFile = File(imagePath);
-    return  await imageFile.readAsBytes();
+    return await imageFile.readAsBytes();
   }
 }
