4ca31a17c23612e4e2e3d0c55bf2e434daebeab8
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 19:29:52 +0300

    add get Image on file utils

--- lib/src/file_utils.dart
+++ lib/src/file_utils.dart
@@ -1,1 +1,11 @@
-class FileUtils {}
+import 'dart:io';
+
+import 'package:image/image.dart';
+
+class FileUtils {
+  static Future<Image?> getImage(String imagePath) async{
+    final imageFile = File(imagePath);
+    final imageBytes = await imageFile.readAsBytes();
+    return decodeImage(imageBytes);
+  }
+}
