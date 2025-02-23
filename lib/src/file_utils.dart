b466edc2b3d61e7d0dd6522d719a825acfaef538
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-23 20:58:50 +0300

    upd docs

--- lib/src/file_utils.dart
+++ lib/src/file_utils.dart
@@ -4,15 +4,18 @@ import 'dart:typed_data';
 import 'package:image/image.dart';
 
 class FileUtils {
+  /// get image from bytes method
   static Future<Image?> getImage(Uint8List imageBytes) async {
     return decodeImage(imageBytes);
   }
 
+  /// get file bytes from file path
   static Future<Uint8List> getFileBytes(String imagePath) async {
     final imageFile = File(imagePath);
     return await imageFile.readAsBytes();
   }
 
+  /// write file bytes on file path
   static Future<void> writeFile(String filePath, List<int> fileBytes) async {
     if (fileBytes.isNotEmpty) {
       final outputFile = File(filePath);
